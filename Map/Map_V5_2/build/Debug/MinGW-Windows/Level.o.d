ed2e92a1c626f2d6f2a0b2867ec8f9c0590555e2
espinozahector <espinozahector68@gmail.com>
2016-09-28 18:32:17 -0700

    Played with map builder, fixed == problem, added some comments
    Signed-off-by: espinozahector <espinozahector68@gmail.com>

---
+++ Map/Map_V5_2/build/Debug/MinGW-Windows/Level.o.d
@@ -0,0 +1,7 @@
+build/Debug/MinGW-Windows/Level.o: Level.cpp Level.h Cols.h Row.h
+
+Level.h:
+
+Cols.h:
+
+Row.h:
---
+++ Map/Map_V5_2/build/Debug/MinGW-Windows/Room.o.d
@@ -0,0 +1,9 @@
+build/Debug/MinGW-Windows/Room.o: Room.cpp Room.h Level.h Cols.h Row.h
+
+Room.h:
+
+Level.h:
+
+Cols.h:
+
+Row.h:
