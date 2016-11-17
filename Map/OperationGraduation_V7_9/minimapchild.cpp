45c361a2ff50748f8347e81e95ee01af1e7e2383
espinozahector <espinozahector68@gmail.com>
2016-11-17 01:14:13 -0800

    Signed-off-by: espinozahector <espinozahector68@gmail.com>

---
+++ Map/OperationGraduation_V7_9/minimapchild.cpp
@@ -0,0 +1,55 @@
+#include "minimapchild.h"
+
+MiniMapChild::MiniMapChild(QWidget *parent) :
+    QMdiSubWindow(parent)
+{
+    this->setFixedSize(350,150);
+//    this->setFixedSize(350,150);
+//    this->setStyleSheet("background-color: #000000;");
+
+//    qDebug() << "Setting QLabel";
+
+//    tacobell = new QLabel[81];
+//    this->setWidget(tacobell);
+
+//    setWidget(tacobell);
+//    tacobell->setStyleSheet("background-color: #000;");
+
+//    for(int i = 0; i<9; i++){
+//        for(int j = 0; j<9; j++){
+//            tacobell[(i*j)+j].setStyleSheet("background-color:#333;");
+//            tacobell[(i*j)+j].setGeometry(i*35,j*15,35,15); //Moves and sets the size
+//            tacobell[(i*j)+j].setSizePolicy(QSizePolicy::Fixed,QSizePolicy::Fixed);
+////            tacobell[(i*j)+j].setVisible(false);
+//        }
+//    }
+
+//    for(int i = 0; i<9; i++){
+//        for(int j = 0; j<9; j++){
+//            tacobell[(i*j)+j].setStyleSheet("background-color: #115285;");
+//        }
+//    }
+//    tacobell[80].setStyleSheet("background-color: #115285;");
+//    tacobell->style()->unpolish(tacobell);
+//    tacobell->style()->polish(tacobell);
+//    tacobell->update();
+}
+MiniMapChild::~MiniMapChild()
+{
+//    delete tacobell;
+}
+
+//void MiniMapChild::update(int x, int y)
+//{
+////    tacobell[40].setStyleSheet("background-color: #115285;");
+//}
+
+//void MiniMapChild::clearTacos()
+//{
+////    for(int i = 0; i<9; i++){
+////        for(int j = 0; j<9; j++){
+////            tacobell[(i*j)+j].setVisible(false);
+////        }
+////    }
+////    tacobell[(4*4)+4].setVisible(true);
+//}
