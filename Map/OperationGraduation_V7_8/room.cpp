61b14365004e640f9949c61e9b4e1e1aa427c040
espinozahector <espinozahector68@gmail.com>
2016-11-16 20:05:12 -0800

    Fixed... I think Signed-off-by: espinozahector <espinozahector68@gmail.com>

---
+++ Map/OperationGraduation_V7_8/room.cpp
@@ -0,0 +1,252 @@
+/*
+ * File:   Room.cpp
+ * Author: Victor Alcaraz
+ * Created on September 25, 2016, 12:27 PM
+ * Purpose:
+ */
+
+#include "Room.h"
+#include <QtDebug>
+
+Room::Room()
+{
+    size = 0;
+}
+
+Room::Room(int x, int y, int nRows, int nCols)
+{
+    bool doors[4] = {0,0,0,0};
+    rRows = nRows;
+    rCols = nCols;
+    size = 0;
+    setLoc(x,y);
+    bldRoom();
+    size++;
+}
+
+Room::Room(int row, int col) : Level(row,col)
+{
+    size = 0;
+}
+
+Room::~Room()
+{
+    if(size >= 1)
+    {
+        for(int i = 0; i < rRows; i++)
+        {
+            delete room[i];
+        }
+        delete [] room;
+    }
+}
+
+//Start room should be open
+void Room::startRoom()
+{
+    vector<pair<int,int> > start;
+    bool create = false;
+
+    start.push_back(make_pair(1,1));
+    start.push_back(make_pair(2,1));
+    start.push_back(make_pair(1,2));
+
+    start.push_back(make_pair(6,1));
+    start.push_back(make_pair(7,1));
+    start.push_back(make_pair(7,2));
+
+    start.push_back(make_pair(1,12));
+    start.push_back(make_pair(1,13));
+    start.push_back(make_pair(2,13));
+
+    start.push_back(make_pair(7,12));
+    start.push_back(make_pair(7,13));
+    start.push_back(make_pair(6,13));
+
+
+    for(int x = 1; x < rRows - 1; x++)
+    {
+        for(int y = 1; y < rCols - 1; y++)
+        {
+            for(unsigned int i = 0 ; i < start.size();i++)
+            {
+                create = false;
+                if(x == start[i].first && y == start[i].second)
+                {
+                    room[x]->setEl(y,0);
+                    create = true;
+                    break;
+                }
+            }
+            if(!create)
+            {
+                room[x]->setEl(y,1);
+            }
+        }
+    }
+}
+
+void Room::setDoors(bool temp[])
+{
+    for(int i = 0; i < 4; i++)
+        doors[i] = temp[i];
+
+//    cout << "doors dynamic array: ";
+//    cout << getXLoc() << "," << getYLoc() << " doors = ";
+//    for(int i = 0; i < 4;i++)
+//    {
+//        cout << doors[i] << " ";
+//    }
+//    cout << endl;
+}
+
+/*
+ * iterate -> x -> y -> coords
+ * bool check
+ *      if blocks < numBlocks check = false;
+ *      if blocks == numBlocks check = true;
+ *
+ *      if check == true
+ *              iterate throught the rest and set to 1
+ *
+ *
+ */
+void Room::bldRoom()
+{
+    //Used to not block doors...
+    vector<pair<int,int> > coordinates;
+
+    //doors
+
+    //walls
+
+    //something else
+    coordinates.push_back(make_pair(0,7));
+    coordinates.push_back(make_pair(1,6));
+    coordinates.push_back(make_pair(1,7));
+    coordinates.push_back(make_pair(1,8));
+    coordinates.push_back(make_pair(2,7));
+    coordinates.push_back(make_pair(8,7));
+    coordinates.push_back(make_pair(3,13));
+    coordinates.push_back(make_pair(4,12));
+    coordinates.push_back(make_pair(4,13));
+    coordinates.push_back(make_pair(5,13));
+    coordinates.push_back(make_pair(4,0));
+    coordinates.push_back(make_pair(6,7));
+    coordinates.push_back(make_pair(7,6));
+    coordinates.push_back(make_pair(7,7));
+    coordinates.push_back(make_pair(7,8));
+    coordinates.push_back(make_pair(4,14));
+    coordinates.push_back(make_pair(3,1));
+    coordinates.push_back(make_pair(4,1));
+    coordinates.push_back(make_pair(4,2));
+    coordinates.push_back(make_pair(5,1));
+
+    float crtPrc;
+    float pTemp;
+    int blocks=0;
+    int numblocks = 10;
+    bool create = false;
+    int divider = 3;
+    room = new Row *[rRows];
+
+    for(int i = 0; i < rRows; i++)
+    {
+        room[i] = new Row(rCols);
+    }
+
+    if(getXLoc() == 4 && getYLoc() == 4)
+    {
+        startRoom();
+        qDebug() << "start room";
+        qDebug() << getXLoc() << "," << getYLoc();
+    }
+    else
+    {
+        for(int x = 1; x < rRows ; x++)
+        {
+            for(int y = 1; y < rCols; y++)
+            {
+                if(x != 8 && y != 14)
+                {
+                    for(unsigned int i = 0 ; i < coordinates.size();i++)
+                    {
+                        if(x == coordinates[i].first && y == coordinates[i].second)
+                        {
+                            room[x]->setEl(y,1);
+                            create = true;
+                            break;
+                        }
+                    }
+                    if(!create)
+                    {
+                        if(blocks < numblocks)
+                        {
+                            if(blocks >=5)
+                            {
+                                if(blocks%2 == 0)
+                                {
+                                    divider++;
+                                }
+                                pTemp = rand();
+                                crtPrc = rand()/divider;
+                            }
+                            else
+                            {
+                                pTemp = rand();
+                                crtPrc = rand()/3;
+                            }
+
+                            if (pTemp <= crtPrc)
+                            {
+                                room[x]->setEl(y,0);
+                                blocks++;
+                            }
+                            else
+                            {
+                                room[x]->setEl(y,1);
+                            }
+                        }
+                        else
+                        {
+                            room[x]->setEl(y,1);
+                        }
+                    }
+                    else
+                    {
+                        room[x]->setEl(y,1);
+                    }
+                    create = false;
+                }
+            }
+        }
+    }
+
+//    cout << "Room [" << this->getXLoc() << "," << this->getYLoc() << "] blocks: " << blocks << endl;
+
+}
+
+void Room::prntRoom()
+{
+    cout << "location: [" << this->getXLoc() << "," << this->getYLoc() << "]\n";
+    for(int x = 0; x < rRows; x++)
+    {
+        for(int y = 0; y < rCols; y++)
+        {
+            if(room[x]->getEl(y)) cout << "1 ";
+            else cout << "0 ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+void Room::setLoc(int x,int y)
+{
+    xLoc = x;
+    yLoc = y;
+}
+
+bool Room::getRoomVal(int x,int y)
+{
+    return room[x]->getEl(y);
+}
---
+++ Map/OperationGraduation_V7_8/tile.cpp
@@ -0,0 +1,81 @@
+#include "tile.h"
+
+Tile::Tile(QGraphicsItem *parent) : QGraphicsPixmapItem(parent)
+{
+       //do nothing but create the tile
+}
+
+void Tile::move(int x, int y)
+{
+    this->setPos((x*60),(y*60));
+}
+
+void Tile::design(int x, int y, bool *a)
+{
+    //Set the tile image
+
+    //SETS the doors
+    if(x == 7 && y == 0 && a[0])
+    {
+        setPixmap(QPixmap(":/Map/images/map/classDoor_top.png"));
+        this->setScale(0.25);
+    }
+    else if(x == 7 && y == 8 && a[2])
+    {
+        setPixmap(QPixmap(":/Map/images/map/classDoor_bot.png"));
+        this->setScale(0.25);
+    }
+    else if(x == 0 && y == 4 && a[3])
+    {
+        setPixmap(QPixmap(":/Map/images/map/classDoor_left.png"));
+        this->setScale(0.25);
+    }
+    else if(x == 14 && y ==4 && a[1])
+    {
+        setPixmap(QPixmap(":/Map/images/map/classDoor_right.png"));
+        this->setScale(0.25);
+    }
+
+    //SETS WALLS
+    else if(x == 0)
+    {
+        setPixmap(QPixmap(":/Map/images/map/classWall_left.png"));
+        this->setScale(0.25);
+    }
+    else if(x == 14)
+    {
+        setPixmap(QPixmap(":/Map/images/map/classWall_right.png"));
+        this->setScale(0.25);
+    }
+    else if(y == 0)
+    {
+        setPixmap(QPixmap(":/Map/images/map/classWall_top.png"));
+        this->setScale(0.25);
+    }
+    else if(y == 8)
+    {
+        setPixmap(QPixmap(":/Map/images/map/classWall_bot.png"));
+        this->setScale(0.25);
+    }
+
+    //SETS FLOOR
+    else if(val == 1)
+    {
+        setPixmap(QPixmap(":/Map/images/map/classFloor3.png"));
+        this->setScale(0.25);
+    }
+    else if(val == 0)
+    {
+        setPixmap(QPixmap(":/Map/images/map/classFloor3_cracked.png"));
+        this->setScale(0.25);
+    }
+    else
+    {
+        setPixmap(QPixmap(":/Map/images/map/errorTile.png"));
+    }
+}
+
+void Tile::setVal(int v)
+{
+    val = v;
+}
