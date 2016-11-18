72d6813c910689894b820820be4ca71827dcb066
vctralcaraz <vctralcaraz@gmail.com>
2016-11-17 22:07:20 -0800

    Database is online and able to accept new usernames and passwords. Signed-off-by: vctralcaraz <vctralcaraz@gmail.com>

---
+++ Map/OperationGraduation_V8_1/mainwindow.h
@@ -0,0 +1,55 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QCryptographicHash>
+#include "roomchild.h"
+#include "loginchild.h"
+#include "statchild.h"
+#include "minimapchild.h"
+#include "connection.h"
+#include "registerchild.h"
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+    Connection createConnection();
+
+private:
+    QGraphicsScene *scene;
+
+    Ui::MainWindow *ui;
+    RoomChild  *roomchild;
+    LoginChild *loginchild;
+    StatChild *statchild;
+//    MiniMapChild *minichild;
+    Connection connection;
+    RegisterChild *regchild;
+    bool isRoom;
+    bool isLogin;
+    bool isStat;
+    bool isNUser;
+
+private slots:
+
+public slots:
+//    void crMnCh();   //creates the minimap child
+    void lgnScrn();  //creates the login screen
+    void crRmCh();   //creates the room child
+    void crStCh();   //creates the status child
+    void newGame();  //creates the game
+    void newUser();  // creates new user screen
+    void goBack();   // going back to login screen from new user screen
+    void validate(); // validate username check
+    void checkUser();// checks username and password credentials
+};
+
+#endif // MAINWINDOW_H
---
+++ Map/OperationGraduation_V8_1/splash.cpp
@@ -0,0 +1,29 @@
+#include "splash.h"
+
+Splash::Splash(QWidget *parent) : QSplashScreen(parent)
+{
+    version = "<h3>Alpha Version 8.1</h3>";
+    this->setPixmap(QPixmap(":/Other/images/other/Title.png"));
+    this->setFocus();
+    this->show();
+}
+
+void Splash::update1()
+{
+    Qt::Alignment topRight = Qt::AlignRight | Qt::AlignTop;
+    this->showMessage(QObject::tr("<h3>Setting up the main window...</h3>"),
+                      topRight, Qt::white);
+}
+
+void Splash::update2()
+{
+    Qt::Alignment topRight = Qt::AlignRight | Qt::AlignTop;
+    this->showMessage(QObject::tr("<h3>Loading the main window...</h3>"),
+                      topRight, Qt::white);
+}
+
+void Splash::showVersion()
+{
+    this->version = version;
+    this->showMessage(this->version,Qt::AlignLeft,Qt::black);
+}
