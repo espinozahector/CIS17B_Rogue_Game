ac56a49f6325f59f3b42a2bc3ca58fde889883ac
espinozahector <espinozahector68@gmail.com>
2016-11-30 19:50:23 -0800

    Fixed statchild,movement within levels Signed-off-by: espinozahector <espinozahector68@gmail.com>

---
+++ Integrated/OperationGraduation_Integrated_V3/bombbullet.cpp
@@ -0,0 +1,23 @@
+#include "bombbullet.h"
+
+#include <QTimer>
+#include <QGraphicsScene>
+
+BombBullet::BombBullet(QGraphicsItem *parent):
+    QObject(), QGraphicsPixmapItem(parent)
+{
+    //Draw graphics
+    setPixmap(QPixmap(":/Character/images/characters/boombullet2.png"));
+    //Size 50, 50
+
+    //Set timer for despawn
+    QTimer *timer = new QTimer();
+    connect(timer, SIGNAL(timeout()),
+            this,SLOT(despawn()));
+    timer->start(1000);
+}
+
+void BombBullet::despawn(){
+    scene()->removeItem(this);
+    delete this;
+}
---
+++ Integrated/OperationGraduation_Integrated_V3/pjockold.h
@@ -0,0 +1,39 @@
+#ifndef PJOCKOLD_H
+#define PJOCKOLD_H
+/*
+#include <QGraphicsPixmapItem>
+#include <QMediaPlayer>
+#include <QTimer>
+
+class PJockold:
+        public QGraphicsPixmapItem,
+        public Player
+{
+private:
+    //String for player class
+    const string PJOB = "Jock";
+    //Name generator for items
+    string genName(int);
+
+    int vel;
+    bool stun;
+
+public:
+    PJock(string, QGraphicsItem *parent=0);
+    void keyPressEvent(QKeyEvent * event);
+
+    //Returns class name
+    string getClass(){return PJOB;}
+
+    //Sets item into the inventory
+    //bool grabItem(Item);
+
+    //Player attack functions
+    int attack1(Character&);
+    int attack2(Character&);
+
+public slots:
+    void checkCollision();
+};
+*/
+#endif // PJOCKOLD_H
